c19e4ae4af9a2771f08aa80e2a6e6bf3e61c1ad8
sujalraval <88503112+sujalraval@users.noreply.github.com>
2026-08-11 15:55:07 +0530

    feat(WebStartup): add new fields for incubation details and innovation USP

---
+++ prisma/migrations/20260811083725_add_startup_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "WebStartup" ADD COLUMN     "dateOfIncubation" TIMESTAMP(3),
+ADD COLUMN     "innovationUSP" TEXT,
+ADD COLUMN     "schemeParticipation" TEXT,
+ADD COLUMN     "stageAtIncubation" TEXT;
